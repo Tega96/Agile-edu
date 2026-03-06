889b2f7216b488488d0ef78abb90d7e1a43c0df9
Otega Otite <otiteotega@gmail.com>
2026-03-06 14:39:08 +0100

    Implement routing for the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, ExamDashboard, LandingPage, Leaderboard, LearningDashboard, Settings, UserProfile } from "./components/pages";
+import Login from "./components/pages/Auth/Login";
+import SignUp from './components/pages/Auth/SignUp'
+
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+
+        {/** Public Routes */}
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="sign-up" element={<SignUp />} />
+        </Routes>
+
+        {/** Protected Routes */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="exam" element={<ExamDashboard /> } />
+          <Route path="rank" element={<Leaderboard />} />
+          <Route path="learn" element={<LearningDashboard />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="profile" element={<UserProfile />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   )
 }
